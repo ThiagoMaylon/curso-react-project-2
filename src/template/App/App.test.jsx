134142b2882713e87622c97b02bc5a33a2cae182
Thiago Maylon <tmaylon88@gmail.com>
2023-01-10 13:34:42 -0300

    fazendo o teste de estilo

---
+++ src/template/App/App.test.jsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react';
+import Home from '.';
+import {renderTheme} from '../../styles/render-theme'
+
+
+test('renders learn react link', () => {
+  const {debug} = renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', {name: 'olá mundo'}).parentElement;
+
+  expect(headingContainer).toHaveStyle({
+    background: 'red',
+  })
+
+  expect(headingContainer).toMatchSnapshot();
+});
